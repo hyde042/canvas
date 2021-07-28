8ef242260b77cb74071516680b8132aea54a0c5f
Pasi Hyvönen <pasi@koud.fi>
2021-07-28 22:51:32 +0300

    Add pdf lib for pdfcanvas

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hyde042/canvas
+
+go 1.16
+
+require github.com/signintech/gopdf v0.9.20
--- pdfcanvas/canvas.go
+++ pdfcanvas/canvas.go
@@ -4,6 +4,8 @@ import (
 	"image"
 
 	"github.com/hyde042/canvas"
+
+	"github.com/signintech/gopdf"
 )
 
 var _ interface {
@@ -12,31 +14,43 @@ var _ interface {
 } = &Canvas{}
 
 type Canvas struct {
-
-	// ???
-
+	doc gopdf.GoPdf
 }
 
-func New( /* TODO: page size argument */ ) *Canvas {
-	return &Canvas{
+func New( /* TODO: options */ ) *Canvas {
+	var c Canvas
+	c.doc.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
 
-		// ???
+	// TODO: load default font(s) lazily
 
+	fontData, err := canvas.FontFS.ReadFile("font/LiberationSerif-Regular.ttf")
+	if err != nil {
+		panic(err)
 	}
+	if err := c.doc.AddTTFFontData("", fontData); err != nil {
+		panic(err)
+	}
+	c.SetFont("", canvas.DefaultFontSize)
+	return &c
 }
 
-func (c *Canvas) Move(image.Point) {
-	panic("TODO")
+func (c *Canvas) Move(p image.Point) {
+	c.doc.SetX(float64(p.X))
+	c.doc.SetY(float64(p.Y))
 }
 
 func (c *Canvas) Draw(img image.Image, rect *image.Rectangle) {
-	panic("TODO")
+
+	// TODO
+
 }
 
 func (c *Canvas) SetFont(name string, size int) {
-	panic("TODO")
+	c.doc.SetFont(name, "", size)
 }
 
 func (c *Canvas) Text(s string) {
-	panic("TODO")
+
+	// TODO
+
 }
