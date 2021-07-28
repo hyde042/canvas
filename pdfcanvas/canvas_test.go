94d68f5842bbf5b0f7b9184957a369722e3b6cbc
Pasi Hyvönen <pasi@koud.fi>
2021-07-28 23:23:03 +0300

    Improve pdfcanvas test

--- pdfcanvas/canvas_test.go
+++ pdfcanvas/canvas_test.go
@@ -10,9 +10,12 @@ import (
 
 func TestCanvas(t *testing.T) {
 	c := pdfcanvas.New()
-	c.Move(image.Point{30, 30})
-	c.Text("Hello, world!")
 
+	c.SetFont("", 24)
+	for i := 1; i <= 100; i++ {
+		c.Move(image.Point{30, i * 30})
+		c.Text("Hello, world!")
+	}
 	if err := os.MkdirAll("temp", 0777); err != nil {
 		t.Fatal(err)
 	}
