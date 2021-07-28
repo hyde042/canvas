1f0746240915c5f6a8f96e91e819756e2df428df
Pasi Hyvönen <pasi@koud.fi>
2021-07-29 01:30:52 +0300

    Handle pages

--- render.go
+++ render.go
@@ -9,11 +9,11 @@ func Render(c Canvas, l layout.Layout) {
 			setFont(c, v)
 		case layout.Text:
 			drawText(c, v)
+		case layout.Page:
+			addPage(c)
 		}
 
 		// TODO: image
-		// TODO: page
-
 	}
 }
 
@@ -28,5 +28,11 @@ func setFont(c Canvas, f layout.Font) {
 func drawText(c Canvas, t layout.Text) {
 	if tc, ok := c.(TextCanvas); ok {
 		tc.DrawText(t.Position, t.Text)
+	}
+}
+
+func addPage(c Canvas) {
+	if pc, ok := c.(PagedCanvas); ok {
+		pc.AddPage()
 	}
 }
